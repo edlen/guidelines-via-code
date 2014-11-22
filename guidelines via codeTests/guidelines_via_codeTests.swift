baf76a85009b860222928988401d9dae81406ec6
Edel <edlen50@gmail.com>
2014-11-22 11:43:57 +0000

    Initial Commit

---
+++ guidelines via codeTests/guidelines_via_codeTests.swift
@@ -0,0 +1,36 @@
+//
+//  guidelines_via_codeTests.swift
+//  guidelines via codeTests
+//
+//  Created by Edel Leonard on 22/11/2014.
+//  Copyright (c) 2014 Edel Leonard. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class guidelines_via_codeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
